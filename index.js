f07fdab7d19e3f2ada10fb42d9aa37e87ceba3a2
Ivan Bosch <ivan.bosch@city.ac.uk>
2021-04-10 12:08:21 +0100

    Added join to chosen rooms and they correctly communicate

--- index.js
+++ index.js
@@ -17,12 +17,18 @@ app.get('', (req, res) => {
 });
 
 io.on('connection', (socket) => {
+
   console.log("A user has connected");
   
-  socket.on("transformation", (data) => {
-    console.log("transformation received");
-    socket.broadcast.emit('transformation', data);
-  }) 
+  socket.on("transformation", ({room, move}) => {
+    console.log("transformation received on: " + room);
+    socket.to(room).emit('transformation', move);
+  })
+
+  socket.on("joinRoom", (data) => {
+    console.log("A user just joined: " + data);
+    socket.join(data);
+  })
 }); 
 
 server.listen(3000, () => {
--- src/js/register.js
+++
@@ -1,8 +0,0 @@
-let usernameAlreadySelected = false;
-
-function onUsernameSelection() {
-    const username = document.getElementById("userName").value;
-    this.usernameAlreadySelected = true;
-    socket.auth = { username };
-    socket.connect();
-}
